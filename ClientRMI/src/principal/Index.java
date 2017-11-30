2fd9d97bb625868e88bd9fda9e82392313b888ea
nikolasbrandao <nikolasbrandao@gmail.com>
2017-11-30 12:47:06 -0300

    criação dos metodos iniciais da radio

--- ClientRMI/src/principal/Index.java
+++ ClientRMI/src/principal/Index.java
@@ -1,11 +1,14 @@
 package principal;
 
+import java.awt.List;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import interfaces.IRadioService;
+import model.Album;
 
 public class Index {
 
@@ -14,7 +17,11 @@ public class Index {
 	public static void main(String[] args) {
 		try {
 			rockstarRadio = (IRadioService) Naming.lookup("//localhost/rockstarradio");
-			rockstarRadio.tocaMusica(0, null);
+			ouve(rockstarRadio.sintoniza());
+			ArrayList<Album> albuns = (ArrayList<Album>) rockstarRadio.listaAlbuns("Kanye West");
+			for (Album album : albuns) {
+				System.out.println(album.getNomeDoAlbum());
+			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
@@ -23,5 +30,9 @@ public class Index {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static void ouve(String mensagem) {
+		System.out.println(mensagem);
 	}
 }
